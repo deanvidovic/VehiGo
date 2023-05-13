45cefab56e09ecf7247db1947edfe8f66a400c54
Hrvooje <cuckovichrvoje@gmail.com>
2023-05-13 19:10:31 +0200

    13.5.2023
    Dodano forme za prijavu i registraciju

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 // importi
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-const dbURI = 'mongodb+srv://admin:vehigoadmin@vehigo.yluvl2q.mongodb.net/VehiGo?retryWrites=true&w=majority'
+const dbURI =
+  "mongodb+srv://admin:vehigoadmin@vehigo.yluvl2q.mongodb.net/VehiGo?retryWrites=true&w=majority";
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => {
-        app.listen(3000, () => {
-            console.log('server je pokrenut na portu 3000');
-        })
-        console.log("baza je uspjesno spojena!")
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    app.listen(3000, () => {
+      console.log("server je pokrenut na portu 3000");
+    });
+    console.log("baza je uspjesno spojena!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+app.get("/cars", (req, res) => {
+  res.render("./sites/cars");
+});
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+app.get("/signin", (req, res) => {
+  res.render("./sites/signin");
+});
 
-
-app.get('/signin', (req, res) => {
-    res.render('sites/signin');
-})
+app.get("/signup", (req, res) => {
+  res.render("./sites/signup");
+});
