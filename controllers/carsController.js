4ded3289511cb03a7e366e53f07d527fb562344a
Dean Vidovic <vidovicdean@gmail.com>
2023-05-29 17:34:19 +0200

    single page

--- controllers/carsController.js
+++ controllers/carsController.js
@@ -13,11 +13,18 @@ module.exports.cars_get = async (req, res) => {
 
 module.exports.single_car = async (req, res) => {
   try {
+    const id = req.params.id;
     const collection = mongoose.connection.collection('cars'); 
     const cars = await collection.find().toArray();
+    const car = cars.find((car) => car._id.toString() === id);
     
-    res.send("Single car")
+    if (car) {
+      res.render('../views/sites/singleCar', { car });
+    } else {
+      console.log('Document not found');
+      res.send('Document not found');
+    }
   } catch (err) {
-    console.log("Greska pri dohvacanju")
+    console.log(err);
   }
 };
